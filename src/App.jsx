721814fcdebb9700a4f6c6dfbca693c13c3091f1
md sanowar alam <94783739+Sanowar72@users.noreply.github.com>
2022-07-18 16:42:02 +0530

    missing file added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// import HomePage from './BMICalculator/HomePage'
+// import MainPage from './Cart/MainPage.jsx';
+import HomeGallery from './Gallery/HomeGallery'
+
+function App() {
+
+  return (
+    // For Shopping Cart
+    // <MainPage /> 
+    
+    <HomeGallery />
+  );
+}
+
+export default App;
